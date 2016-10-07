72933e72828d8deb236ff4447eba240bbc2c5b87
Valko Vova <valko.volodya@gmail.com>
2016-10-08 02:49:30 +0300

    Sync commit

--- app/application.py
+++ app/application.py
@@ -4,7 +4,7 @@ from flask import Flask
 from .routes import setup_routes
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_url_path='/work/static')
     filepath = os.getenv('APP_CONFIG')
     abspath = os.path.abspath(filepath)
     app.config.from_pyfile(abspath)
--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 import os
 from app.application import create_app
 
+app = create_app()
+
 if __name__ == '__main__':
-    app = create_app()
     app.run(
         host=os.environ.get('HOST', '0.0.0.0'),
         port=int(os.environ.get('PORT', 5000))
