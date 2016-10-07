9c6cf553523ff0ec7daeca4cbbfedb89a31b7765
Oleksandr Kovalchuk <anxolerd@outlook.com>
2016-10-08 01:16:48 +0300

    Add a dummy adventure view page
    Create a dummy view to display adventure by its id.
    As there is no mongo integration here right now, It is just a view stub
    with TODO =)

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
 from .views.index import index_view
+from .views.index import adventure_view
 
 
 def setup_routes(app):
     app.add_url_rule('/', 'index', view_func=index_view)
+    app.add_url_rule('/adventure/<int:id_>', 'adventure_view', view_func=adventure_view)
--- app/views/index.py
+++ app/views/index.py
@@ -3,3 +3,11 @@ from flask import render_template
 
 def index_view():
     return render_template('index.html')
+
+
+def adventure_view(id_):
+    # TODO: get adventure by id
+    return render_template(
+        'view_adventure.html',
+        adventure_id=id_,
+    )
