966a1caacd75d1305ba48f295f2f38875607e3b1
Oleksandr Kovalchuk <anxolerd@outlook.com>
2016-10-08 05:20:57 +0300

    [Fix] Fix calls to mongo
    After refactoring there is no db attribute in app. There is a global
    variable called `mongo` Use it instead of app.db

--- app/bl/adventure_resource.py
+++ app/bl/adventure_resource.py
@@ -1,16 +1,16 @@
-from flask import current_app as app
-from ..utils import objectid_fix
+from app import mongo
+from app.utils import objectid_fix
 
 
 def create_adventure(*args, **kwargs):
-    app.db.xture.insert(**kwargs)
+    mongo.db.xture.insert(**kwargs)
 
 
 def get_adventure_by_id(_id):
-    adv = app.db.xture.find_one({'_id': _id})
+    adv = mongo.db.xture.find_one({'_id': _id})
     return objectid_fix(adv)
 
 
 def get_list_of_adventures():
-    ls_ = list(app.db.xture.find_all())
+    ls_ = list(mongo.db.xture.find_all())
     return objectid_fix(ls_)
