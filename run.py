d2eca4b264283c4c096cba7ebc6840c7dd1085d0
Valko Vova <valko.volodya@gmail.com>
2016-10-08 03:05:03 +0300

    Dummy changes

--- run.py
+++ run.py
@@ -5,7 +5,6 @@ from flask import g
 app = create_app()
 
 if __name__ == '__main__':
-    app = create_app()
     app.run(
         host=os.environ.get('HOST', '0.0.0.0'),
         port=int(os.environ.get('PORT', 5000))
