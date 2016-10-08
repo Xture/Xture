5ec0f9ac2d6ac79c086ab4e077179bd06d255d09
Oleksandr Kovalchuk <anxolerd@outlook.com>
2016-10-08 07:27:20 +0300

    Fix codestyle for app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,6 @@ from flask_script import Manager
 from flask_pymongo import PyMongo
 
 
-
 app = Flask(__name__, static_folder='../static')
 manager = Manager(app)
 filepath = os.getenv('APP_CONFIG')
@@ -15,13 +14,13 @@ app.config.from_pyfile(abspath)
 
 # set up custom json encoder to serialize/deserialize
 # ObjectID properly
-from app.utils import JSONEncoder
+from app.utils import JSONEncoder  # NOQA
 app.json_encoder = JSONEncoder
 
 # mongodb setup
 mongo = PyMongo(app)
 
-from app.routes import setup_routes
-from app.routes import RegexConverter
+from app.routes import setup_routes  # NOQA
+from app.routes import RegexConverter  # NOQA
 app.url_map.converters['regex'] = RegexConverter
 setup_routes(app)
